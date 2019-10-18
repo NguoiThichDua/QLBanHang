957fbc6ddf86026c2a13a6c43f3b6444e56fbd42
Sơn Nguyễn <nbhs.it.98@gmail.com>
2019-10-18 15:43:40 +0700

    Son

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -2,7 +2,9 @@
 $(document).ready(function(e) {
 	// dung ca trang ca nhan, goi form sua tai khoan ma khong can load trang
 	$(".suathongtin").click(function(){
-		var hotenkhachhang = document.getElementById("hotenkhachhang");
+		// lay ten khach hang  de thay doi thong tin tai suakhachhang.php
+		var hotenkhachhang = document.getElementById("hotenkhachhang").innerHTML;
         $("#suathongtincanhan").load("view/component/khachhang/suathongtincanhan.php" , {hotenkhachhang: hotenkhachhang});
+		
 	});
 });
